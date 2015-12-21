26df96cc468cc945ee5fafc7c1f8e7ad7a2f3082
John Castaldo <jcastaldo@Johns-MacBook-Pro.local>
2015-12-20 18:58:11 -0600

    Implemented code to store matrix inverse in method closure and to find and/or set matrix inverse inside that closure

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The makeCacheMatrix function will create a list containing functions that
+## 1. Set the value of the matrix
+## 2. Get the value of the matrix
+## 3. Set the value of the matrix inverse
+## 4. Get the value of the matrix inverse
+## We cache both the matrix inverse and the matrix values inside the makeCacheMatrix closure
+## Note: Since we are utilizing the method closure we must null the stored matrix Inverse when a new value is set
+## Or it's possible to return inverses to a different matrix than the one in question
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(matrix) {
+  matrixInverse <- NULL
+  
+  setMatrix <- function(matrixToSet) {
+    matrix <<- matrixToSet
+    matrixInverse <<- NULL
+  }
+  
+  getMatrix <- function() matrix
+  
+  setMatrixInverse <- function(matrixInverseToSet) matrixInverse <<- matrixInverseToSet
+  
+  getMatrixInverse <- function() matrixInverse
+  
+  list(setMatrix = setMatrix, getMatrix = getMatrix,
+       setMatrixInverse = setMatrixInverse,
+       getMatrixInverse = getMatrixInverse)
 }
 
 
-## Write a short comment describing this function
+## The cacheSolve function will take a matrix value and check to see if the closure of the makeCacheMatrix method
+## contains a value for the matrix inverse. If it does, then that value is returned. If it does not,
+## Then the inverse is computer and set inside the makeCacheMatrix closure
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(cacheMatrix) {
+  matrixInverse <- cacheMatrix$getMatrixInverse()
+  
+  if(!is.null(matrixInverse)) {
+    message("getting cached matrix inverse")
+    return(matrixInverse)
+  }
+  
+  matrix <- cacheMatrix$getMatrix()
+  matrixInverse <- solve(matrix)
+  cacheMatrix$setMatrixInverse(matrixInverse)
+  
+  matrixInverse
 }
